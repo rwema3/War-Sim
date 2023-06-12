81188fcd101175e229324bbc14a6b964275a4b9e
rwemadominik3@gmail.com <52289151+rwema3@users.noreply.github.com>
2023-06-12 21:46:46 +0200

    Echo

--- bin/war_sim.dart
+++ bin/war_sim.dart
@@ -5,3 +5,4 @@ void printAggregateStatistics(List<Stats> gameStats) {
   void printStats(String label, Iterable<int> values) {
     var stats = stats_package.Stats.fromData(values);
     var rounded = stats.withPrecision(3);
+    print("$label: ${rounded.median} (median)");
