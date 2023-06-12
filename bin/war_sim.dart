fee20e97f5c417c5b4d4c3d63b51b701925c277a
rwemadominik3@gmail.com <52289151+rwema3@users.noreply.github.com>
2023-06-12 21:46:40 +0200

    Variable rounded!

--- bin/war_sim.dart
+++ bin/war_sim.dart
@@ -4,3 +4,4 @@ import 'package:war_sim/war_sim.dart';
 void printAggregateStatistics(List<Stats> gameStats) {
   void printStats(String label, Iterable<int> values) {
     var stats = stats_package.Stats.fromData(values);
+    var rounded = stats.withPrecision(3);
