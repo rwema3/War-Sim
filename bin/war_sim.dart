d77ad27a364d9342267fb2bed4fad6471b2ba394
rwemadominik3@gmail.com <52289151+rwema3@users.noreply.github.com>
2023-06-12 21:44:22 +0200

    Update war_sim.dart

--- bin/war_sim.dart
+++ bin/war_sim.dart
@@ -26,5 +26,3 @@ void runSimForRules(Rules rules, String name, {int count = 50000}) {
   print("Simulating with rules \"$name\" N=$count");
   final gameStats = runSimulations(rules, count);
   printAggregateStatistics(gameStats);
-}
-
